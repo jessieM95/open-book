09f6bd5268023b0b8fad35392dd444c5c6848f1e
Peter Maneykowski <pmaneykowski@gmail.com>
2015-12-08 13:54:52 -0600

    password functionality

--- javascript/main.js
+++ javascript/main.js
@@ -3,42 +3,55 @@ document.getElementById("inprogress").addEventListener("click",click);
 document.getElementById("popular").addEventListener("click",click);
 document.getElementById("submit").addEventListener("click",addToPage);
 document.getElementById("create_profile").addEventListener("click",create);
+document.getElementsByClassName("password_warning")[0].style.visibility = "hidden";
 var story = document.getElementById("story");
 var userstories = document.getElementById("p");
-var para = document.getElementsByTagName('p');
+var paragraphs = document.getElementsByTagName("p");
 var title = document.getElementById("myModalLabel");
 var start = document.getElementById("start");
 var create = document.getElementById("create");
+var entry1 = document.getElementsByClassName("entry1");
 var entry = document.getElementsByClassName("entry");
+
+for(var i = 0; i < entry1.length; i++) {
+    entry1[i].onchange = available;
+}
 
 for(var i = 0; i < entry.length; i++) {
     entry[i].onchange = responsiveInput;
 }
 
-function create() {
-    var password_warning = this.parentNode.getElementsByClassName("password_warning");
-   console.log(password_warning);
-   password_warning[0].visibility = 
-    if (entry[2].value != entry[3].value) {
-        // password_warning[0].className = "";
-    }
 
+function available() {
+    var row = this.parentNode.parentNode;
+    var response = row.getElementsByTagName("p")[0];
+    response.innerHTML = 'Available!'
 }
 
 function responsiveInput() {
     var row = this.parentNode.parentNode;
-    var javaS = row.getElementsByTagName("p");
-    javaS[0].innerHTML = this.value;
+    var response = row.getElementsByTagName("p")[0];
 }
+
+function create() {
+    var password_warning = this.parentNode.getElementsByClassName("password_warning");
+    if (entry[0].value === entry[1].value)
+        password_warning[0].style.visibility = "hidden";
+
+    else
+        password_warning[0].style.visibility = "";
+}
+
 function addToPage() {
     var d = 0;
-    if(story.value == "") {
+    if(story.value == "")
         alert("You have to enter some text to post your story!");
-    }
+
     else {
         for(var i = 0; i < 10; i++) {
-            if(para[i].innerHTML == "") {
-                para[i].innerHTML = story.value;
+            if (paragraphs[i].innerHTML == "") {
+                paragraphs[i].innerHTML = story.value;
+                console.log(story.value);
                 p.className = "";
                 story.value = "";
                 break;
